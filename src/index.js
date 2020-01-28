292c7ea2dcca2244087e8ac869f2054071452a8d
Alex Rocha <rocha8524@gmail.com>
2020-01-28 14:36:27 -0500

    Logo's for leagues

--- src/index.js
+++ src/index.js
@@ -1,11 +1,8 @@
+import "./index.css";
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./components/App/app";
-import "./index.css";
+import App from "./components/App/App";
 import { BrowserRouter as Router } from "react-router-dom";
 
 ReactDOM.render(
-    <Router>
-        <App />
-    </Router>,
-    document.getElementById("root"));
+    <Router><App /></Router>,document.getElementById("root"));
