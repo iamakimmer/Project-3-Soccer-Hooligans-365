21a0db1c7bee4ef430d5a2c9c09d450a971982f1
Alex Rocha <rocha8524@gmail.com>
2020-01-28 18:22:31 -0500

    Server working, working on getting cards to display

--- src/components/App/app.js
+++ src/components/App/app.js
@@ -1,9 +1,17 @@
-import React, { Component } from "react";
 import "./style.css";
-import Card from "./components/Card";
-import Header from "./components/Header";
-import NavBar from "./components/NavBar";
-import SelectLeague from "./components/SelectLeague";
-import Standings from "./components/Standings";
-import Wrapper from "./components/Wrapper";
+import { Route } from 'react-router-dom';
+import React, { Component } from "react";
+// import Header from "./components/Header";
+// import Wrapper from "./components/Wrapper";
+import SelectLeague from "../SelectLeague/index"
 
+class App extends Component {
+    render() {
+        return (
+            <div className="App">
+                <Route exact path="/league-selector" Component={SelectLeague} />
+            </div>
+        );
+    }
+}
+export default App;
