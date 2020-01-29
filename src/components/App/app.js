56fa55634d034b8b81b87dcf4bd4b7f270f43e86
Alex Rocha <rocha8524@gmail.com>
2020-01-28 20:13:25 -0500

    Working to display league select page

--- src/components/App/app.js
+++ src/components/App/app.js
@@ -4,12 +4,29 @@ import React, { Component } from "react";
 // import Header from "./components/Header";
 // import Wrapper from "./components/Wrapper";
 import SelectLeague from "../SelectLeague/index"
+import LeagueDetails from "../LeagueDetails/LeagueDetails";
+import BaseService from "../../api/BaseService";
+import SoccerProvider from "../../api/SoccerService";
 
 class App extends Component {
     render() {
         return (
             <div className="App">
-                <Route exact path="/league-selector" Component={SelectLeague} />
+                <SoccerProvider>
+                    <Route exact path="/league-selector" Component={SelectLeague} />
+                    <BaseService.Consumer>
+                        {service => (
+                            <React.Fragment>
+                                <Route
+                                    path="/league"
+                                    render={props => (
+                                        <LeagueDetails services={service} {...props} />
+                                    )}
+                                />
+                            </React.Fragment>
+                        )}
+                    </BaseService.Consumer>
+                </SoccerProvider>
             </div>
         );
     }
--- src/components/SelectLeague/index.js
+++ src/components/SelectLeague/index.js
@@ -1,25 +1,27 @@
-import React from "react";
 import "./style.css";
+import React from "react";
 import { leagues } from "../../league-codes/index"
 
-const SelectLeague = props => (
-    <React.Fragment>
-        <h2>Select the league & See the details</h2>
-        <div className="league-select">
-            {leagues.map(league => (
-                <div
-                    className="league-card-container"
-                    key={league.id}
-                    onClick={() => props.history.push(`/league/${league.code}`)}
-                >
-                    <div className="league-img">
-                        <img src={league.src} alt="" />
+const SelectLeague = props => {
+    return (
+        <React.Fragment>
+            <h2>Select the league of your choice for more information</h2>
+            <div className="league-select">
+                {leagues.map(league => (
+                    <div
+                        className="league-cards-container"
+                        key={league.id}
+                        onClick={() => props.history.push(`/league/${league.code}`)}
+                    >
+                        <div className="league-img">
+                            <img src={league.src} alt="" />
+                        </div>
+                        <div className="league-name">{league.name}</div>
                     </div>
-                    <div className="league-name">{league.name}</div>
-                </div>
-            ))}
-        </div>
-    </React.Fragment>
-);
+                ))}
+            </div>
+        </React.Fragment>
+    );
+}
 
 export default SelectLeague;
