c17a402d07be774489dadf54be641d0a3e945ca2
Alex Rocha <rocha8524@gmail.com>
2020-02-08 16:17:19 -0500

    Ready to pull

--- src/Pages/Scorers.js
+++ src/Pages/Scorers.js
@@ -1,5 +1,4 @@
 import API from '../API';
-import { groupBy } from 'lodash';
 import NavBar from '../Components/NavBar';
 import Loading from '../Components/Loading';
 import React, { useEffect, useState } from 'react';
@@ -8,40 +7,18 @@ function GoalScorers({ goal }) {
     const [score, setGoals] = useState([]);
     const [isLoading, setIsLoading] = useState(false);
     const id = goal.params.id;
-    const [activeCompetitions, setActiveCompetitions] = useState("");
 
     useEffect(() => {
         const fetchScorers = async () => {
             setIsLoading(true);
             const result = await API.get(`competitions/${id}/scorers`);
+
             setGoals(result.data.scorers);
             setIsLoading(false);
         };
 
         fetchScorers();
     }, [id]);
-
-    useEffect(() => {
-        const competitions = groupBy(
-            goal.activeCompetitions,
-            competition => competition.area.name
-        );
-
-        setActiveCompetitions(
-            <div>
-                {Object.keys(competitions).map((name, index) => (
-                    <div key={index}>
-                        <h5>{name}</h5>
-                        <ul>
-                            {competitions[name].map(competition => (
-                                <li key={competition.id}>{competition.name}</li>
-                            ))}
-                        </ul>
-                    </div>
-                ))}
-            </div>
-        );
-    }, [goal]);
 
     return (
         <div>
@@ -50,10 +27,9 @@ function GoalScorers({ goal }) {
             ) : (
                     <div className="Scorers">
                         <NavBar />
-                        {activeCompetitions}
                         <h2>{score.player.id}</h2>
                         >
-                                <h2 key={score.homeTeam.id}>{score.homeTeam.name}</h2>
+                        <h2 key={score.homeTeam.id}>{score.homeTeam.name}</h2>
                         <h3 key={score.id}>{score.score.fullTime.homeTeam} : {score.score.fullTime.awayTeam}</h3>
                         <h2 key={score.awayTeam.id}>{score.awayTeam.name}</h2>
                     </div>
