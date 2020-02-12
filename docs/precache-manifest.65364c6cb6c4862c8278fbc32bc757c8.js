20917e32cb236a061226ee6352efa7f96f9283e0
Alex Rocha <rocha8524@gmail.com>
2020-02-12 17:42:21 -0500

    add docs folder

---
+++ docs/precache-manifest.65364c6cb6c4862c8278fbc32bc757c8.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d018cb658453ae1698521a509e8852a",
+    "url": "/Project-3-Soccer-Hooligans-365/index.html"
+  },
+  {
+    "revision": "d54b7d2ea0e01b5d735a",
+    "url": "/Project-3-Soccer-Hooligans-365/static/css/main.8aad93e1.chunk.css"
+  },
+  {
+    "revision": "130acf022a220101445c",
+    "url": "/Project-3-Soccer-Hooligans-365/static/js/2.87e0d9c0.chunk.js"
+  },
+  {
+    "revision": "adf988142ef3edd1ada16f48aab2de8c",
+    "url": "/Project-3-Soccer-Hooligans-365/static/js/2.87e0d9c0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d54b7d2ea0e01b5d735a",
+    "url": "/Project-3-Soccer-Hooligans-365/static/js/main.3071d318.chunk.js"
+  },
+  {
+    "revision": "96f13b1c43b07b8b704f",
+    "url": "/Project-3-Soccer-Hooligans-365/static/js/runtime-main.92450b12.js"
+  },
+  {
+    "revision": "9a735bda69f231ef2e20ef4c78349920",
+    "url": "/Project-3-Soccer-Hooligans-365/static/media/2000.9a735bda.png"
+  },
+  {
+    "revision": "52417194fb7eaf325d9c237d168b8917",
+    "url": "/Project-3-Soccer-Hooligans-365/static/media/2001.52417194.png"
+  },
+  {
+    "revision": "3e41a0e90db278fdcbfda72c862ffd2b",
+    "url": "/Project-3-Soccer-Hooligans-365/static/media/2016.3e41a0e9.png"
+  },
+  {
+    "revision": "3771cdc58568a3b8a1ec2613de56f53d",
+    "url": "/Project-3-Soccer-Hooligans-365/static/media/2018.3771cdc5.png"
+  },
+  {
+    "revision": "eb8f47e508735a7dd9c9c6ddff350a3f",
+    "url": "/Project-3-Soccer-Hooligans-365/static/media/android-chrome-192x192.eb8f47e5.png"
+  },
+  {
+    "revision": "cda31c011aac1177ee24dd08310ae468",
+    "url": "/Project-3-Soccer-Hooligans-365/static/media/android-chrome-512x512.cda31c01.png"
+  },
+  {
+    "revision": "5435106553b407a26826eaa1c3fe2818",
+    "url": "/Project-3-Soccer-Hooligans-365/static/media/apple-touch-icon.54351065.png"
+  },
+  {
+    "revision": "afca8b29a537e3cc7713739154ea6034",
+    "url": "/Project-3-Soccer-Hooligans-365/static/media/background5.afca8b29.jpg"
+  },
+  {
+    "revision": "7b4f261ffe9029ec325f4043380f784e",
+    "url": "/Project-3-Soccer-Hooligans-365/static/media/logo.7b4f261f.png"
+  }
+]);
